d79eb86121994158a38b6a60d5273906f54bd115
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-05-07 15:04:07 -0500

    moveForward, moveBackard, moveAround, isInGo methods added to Player class

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,20 +1,23 @@
 package game;
 
+import game.cards.Card;
+import game.dices.dicesResult;
+
 public class Player {
 
     String name;
     int turno;
     float money;
-    int x;
+    Card position;
     boolean prisoner;
-    Player  next;
+    Player next;
     Player prev;
 
     public Player(String name, float money) {
         this.name = name;
         this.money = money;
         this.prisoner = false;
-        this.x = 0;
+        this.position = null;
         prev = this;
         next = this;
     }
@@ -34,6 +37,27 @@ public class Player {
 
     public void goJail() {
         this.prisoner = true;
-        this.x = 10;
+        // this.position = 10;
+    }
+
+    public void moveAround(dicesResult result) {
+        for (int i = 0; i < result.result; i++) {
+            moveForward();
+        }
+    }
+
+    public boolean isInGo() {
+        return position.index == 0;
+    }
+
+    public void moveForward() {
+        position = position.next;
+        if (isInGo()) {
+            // cobrar dinero al pasar por salida
+        }
+    }
+
+    public void moveBackward() {
+        position = position.prev;
     }
 }
