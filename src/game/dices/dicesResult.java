b98480169161181676c19fd641141f856c7fa459
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-05-07 13:15:58 -0500

    result field added to dicesResult class

--- src/game/dices/dicesResult.java
+++ src/game/dices/dicesResult.java
@@ -3,15 +3,17 @@ package game.dices;
 public class dicesResult {
     public int[] results;
     public boolean isPair = false;
+    public int result = 0;
 
-
-    public dicesResult(int[] results, boolean isPair){
+    public dicesResult(int[] results, boolean isPair) {
         this.results = results;
         this.isPair = isPair;
+        this.result = results[0] + results[1];
     }
 
-    public void display(){
-        System.out.println(results[0]+" "+results[1]+" "+ isPair);
+    public void display() {
+        System.out.println(results[0] + " " + results[1] + " " + isPair);
+        System.out.println(result);
     }
-    
+
 }
