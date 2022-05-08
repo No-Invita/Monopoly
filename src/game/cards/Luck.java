7239ba7bb63a11f26d3c851fdbc7a22784de8dcd
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-05-08 16:42:56 -0500

    Luck class

---
+++ src/game/cards/Luck.java
@@ -0,0 +1,14 @@
+package game.cards;
+
+public class Luck extends Card {
+
+    public Luck() {
+        this.name = "Suerte";
+        this.type = "luck";
+        this.isOwnable = false;
+        this.isOwned = false;
+        this.isMortgable = false;
+        this.isMortgabled = false;
+    }
+
+}
